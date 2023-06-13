1223f05e32b86c0edfae767bdaad608f711db19a
Sumaiya786 <sheikhsumaiya.k@gmail.com>
2023-06-13 13:18:13 +0530

    Added code for models 1,2, & 3

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,13 @@
 print("Adding all ML related models")
+
+
+def model1():
+    print("Adding code for model 1")
+
+
+def model2():
+    print("Adding code for model 2")
+
+
+def model3():
+    print("Adding code for model 3")
